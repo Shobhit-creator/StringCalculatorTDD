48e7c65bdfa15dabb76c9b6e547b9ee1098d526a
Shobhit-creator <pshobhitsingh01@gmail.com>
2024-12-15 19:54:02 +0530

    any digit integer test case added

--- StringCalculatorTDD.Tests/StringCalculatorTests.cs
+++ StringCalculatorTDD.Tests/StringCalculatorTests.cs
@@ -9,6 +9,9 @@ public class StringCalculatorTests
     [InlineData("", 0)]
     [InlineData("2", 2)]
     [InlineData("2,3,5,7", 17)]
+    [InlineData("10,15", 25)]
+    [InlineData("100,15", 115)]
+
     public void NumberString_ValidInput_ShouldReturnSum(string input, int expectedResult)
     {
         int result = StringCalculator.Add(input);
