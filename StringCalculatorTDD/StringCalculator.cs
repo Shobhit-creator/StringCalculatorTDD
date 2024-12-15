cfdc3371b34e53f4277dc67e4ce0ab4e3c15a5ff
Shobhit-creator <pshobhitsingh01@gmail.com>
2024-12-15 19:38:10 +0530

    passed numbers sum test case
    passed the failed number sum test

--- StringCalculatorTDD/StringCalculator.cs
+++ StringCalculatorTDD/StringCalculator.cs
@@ -3,6 +3,16 @@ public static class StringCalculator
 {
     public static int Add(string numbers)
     {
-        return 0;
+        if(numbers.Length == 0) return 0;
+        char[] chars = numbers.ToCharArray();
+        int sum = 0;
+        foreach(char ch in chars)
+        {
+            if(ch >= '0' && ch <= '9')
+            {
+                sum += ch - '0';
+            }
+        }
+        return sum;
     }
 }
