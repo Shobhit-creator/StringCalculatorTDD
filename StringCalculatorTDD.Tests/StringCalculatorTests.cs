4e8464d37fb041fd75b064d365b0ebe18fb3c873
Shobhit-creator <pshobhitsingh01@gmail.com>
2024-12-15 18:53:05 +0530

    empty string validation added

--- StringCalculatorTDD.Tests/StringCalculatorTests.cs
+++ StringCalculatorTDD.Tests/StringCalculatorTests.cs
@@ -5,8 +5,12 @@ namespace StringCalculatorTDD.Tests;
 public class StringCalculatorTests
 {
     [Fact]
-    public void Test1()
+    public void EmptyString_NoNumbersToAdd_ShouldReturnZero()
     {
+        string str = "";
 
+        int result = StringCalculator.Add(str);
+
+        Assert.Equal(result, 0);
     }
 }
