c32515b69768df458acc0a145abc4b09a6a62084
Shobhit-creator <pshobhitsingh01@gmail.com>
2024-12-15 20:07:06 +0530

    passed double digit integer test cases

--- StringCalculatorTDD/StringCalculator.cs
+++ StringCalculatorTDD/StringCalculator.cs
@@ -3,16 +3,24 @@ public static class StringCalculator
 {
     public static int Add(string numbers)
     {
+        int n = numbers.Length;
         if(numbers.Length == 0) return 0;
         char[] chars = numbers.ToCharArray();
-        int sum = 0;
-        foreach(char ch in chars)
+        int sum = 0, i= 0, num = 0;
+        while(i < n)
         {
-            if(ch >= '0' && ch <= '9')
+            if(chars[i] == ',')
             {
-                sum += ch - '0';
+                sum += num;
+                num = 0;
             }
+            else
+            {
+                num = chars[i] - '0' + num * 10;
+            }
+            i += 1;
         }
+        sum += num;
         return sum;
     }
 }
