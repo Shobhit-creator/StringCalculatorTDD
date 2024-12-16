8469cff0ebe6a3700b539b4088bc13dfc338081b
Shobhit-creator <pshobhitsingh01@gmail.com>
2024-12-17 01:54:26 +0530

    added newline user input handling

--- StringCalculatorTDD/Program.cs
+++ StringCalculatorTDD/Program.cs
@@ -4,9 +4,20 @@ class Program
     public static void Main(string[] args)
     {
         Console.Write("Enter numbers to add: ");
+        string numbers = "";
 
-        string numbers = Console.ReadLine();
+        while (true)
+        {
+            string line = Console.ReadLine();
 
+            if (string.IsNullOrEmpty(line))
+            {
+                break;
+            }
+
+            numbers += line + "\n";
+        }
+        Console.WriteLine(numbers);
         int result = StringCalculator.Add(numbers);
         Console.WriteLine($"User entered numbers: {result}");
     }
